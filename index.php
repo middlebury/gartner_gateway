224fd54178729550b00983411308cb7ab8b99468
Adam Franco <afranco@middlebury.edu>
2011-04-27 11:38:47 -0400

    Added attribute validation and group membership checking.

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  */ 
 
+// config
+$institutionGroup = "CN=institution,OU=General,OU=Groups,DC=middlebury,DC=edu";
+
+
+
 require_once('phpcas/source/CAS.php');
 
 phpCAS::client(CAS_VERSION_2_0,'login.middlebury.edu', 443,'/cas', true);
@@ -18,4 +23,68 @@ phpCAS::setCasServerCACert('/etc/pki/tls/certs/ca-bundle.crt');
 // force CAS authentication
 phpCAS::forceAuthentication();
 
-print "Authenticated.";
+
+// Verify that we have the needed attributes.
+$errors = array();
+if (!phpCAS::hasAttribute('FirstName') || !strlen(phpCAS::getAttribute('FirstName')))
+	$errors[] = "Your account has no FirstName.";
+else
+	$firstname = phpCAS::getAttribute('FirstName');
+	
+if (!phpCAS::hasAttribute('LastName') || !strlen(phpCAS::getAttribute('LastName')))
+	$errors[] = "Your account has no LastName.";
+else
+	$lastname = phpCAS::getAttribute('LastName');
+	
+if (!phpCAS::hasAttribute('EMail') || !strlen(phpCAS::getAttribute('EMail')))
+	$errors[] = "Your account has no EMail.";
+else
+	$email = phpCAS::getAttribute('EMail');
+
+// Verify Group membership to exclude guests.
+if (!phpCAS::hasAttribute('MemberOf'))
+	$errors[] = "Error fetching group membership. No groups found.";
+
+$memberOf = phpCAS::getAttribute('MemberOf');
+if (is_string($memberOf)) {
+	if ($memberOf != $institutionGroup)
+		$errors[] = "You aren't a member of the allowed group: institution";
+} else if (is_array($memberOf)) {
+	if (!in_array($institutionGroup, $memberOf))
+		$errors[] = "You aren't a member of the allowed group: institution";
+} else {
+	$errors[] = "Error fetching group membership. No groups found.";
+}
+
+	
+if (count($errors)) {
+	print <<<END
+<html>
+<head>
+	<title>Gartner Login - Error</title>
+</head>
+<body>
+	<h1>Gartner Login</h1>
+	<h2>Authentication Error</h2>
+	<ul>
+END;
+	foreach ($errors as $error) {
+		print "<li>".$error."</li>";
+	}
+	print <<<END
+	</ul>
+</body>
+</html>
+END;
+	exit;
+}
+
+print "Successfully Authenticated.";
+
+/*********************************************************
+ * Build the Gartner URL
+ *********************************************************/
+ 
+ 
+ 
+ 
